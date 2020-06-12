63e8cecc1894eaf64bf9d4bc5e44c892e6fda977
YosiBukris <yosibukris@gmail.com>
2020-06-12 08:20:48 +0100

    Initial Commit

---
+++ MyDoggyWalkerTests/MyDoggyWalkerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyDoggyWalkerTests.swift
+//  MyDoggyWalkerTests
+//
+//  Created by user166548 on 6/12/20.
+//  Copyright © 2020 user166548. All rights reserved.
+//
+
+import XCTest
+@testable import MyDoggyWalker
+
+class MyDoggyWalkerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
